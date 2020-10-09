170e9f1dbe54bcfb53c714c5a0880798412a4070
calebbenjin <calebben94@gmail.com>
2020-10-08 18:20:25 -0700

    complete connection to the database

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,13 @@
 import express from 'express'
 import dotenv from 'dotenv'
+import connectDB from './config/db.js'
 import products from './data/products.js'
 
 dotenv.config()
+
+
+// Initializing Mongoose database
+connectDB()
 
 const app = express()
 
